d77167163e9f6043eeda18f15c248da62c66ca80
Gernot Klobucaric <gernot-tv@gmx.at>
2019-03-10 11:39:39 +0100

    Dockerfile config

--- root/etc/vcontrold/run.sh
+++ root/etc/vcontrold/run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 #/usr/local/edomi/main/vcontrol/vcontrold -x /usr/local/edomi/main/vcontrol/vcontrold.xml -d /dev/ttyUSB0 -l /usr/local/edomi/main/vcontrol/vcontrold.log -p 3002 >/dev/null 2>&1 & echo $!
-vcontrold -x /etc/vcontrold/vcontrold.xml -d /dev/ttyUSB0 -l /tmp/vcontrold.log -p $PORTCLI
+vcontrold -x /etc/vcontrold/vcontrold.xml -d /dev/ttyUSB0 -l /etc/vcontrold/vcontrold.log -p $PORTCLI
 status=$?
 if [ $status -ne 0 ];then
 	echo "Failed to start vcontrold"
-	exit 0
+#	exit 0
 else
- echo "vcontrold gestartet"
+ pid=$(pidof vcontrold)
+# pid=$?
+ echo "vcontrold gestartet PID: $pid"
 fi
+# Prüfintervall ob PIDfile vorhanden sonst Container beenden
+while sleep 5; do
+#	pidof vcontrold
+	pidnow=$(pidof vcontrold)
+	if [ $pid -eq $pidnow ]; then
+		echo "PID OK $pid = $pidnow"
+	else
+		echo "exit 0"
+#		exit 0
+	fi
+done
