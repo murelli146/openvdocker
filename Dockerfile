74e2661bfe2c3c21d078a22218b692ba4b2934c0
Gernot Klobucaric <gernot-tv@gmx.at>
2019-03-14 11:14:05 +0100

    mqtt publish hinzugefügt

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM debian:latest
 MAINTAINER Gernot Klobucaric <murelli146>
+RUN apt-get update && apt-get install -y mosquitto-clients
 COPY root / 
-ENV PORTCLI 3002
+ENV IPMQTTBROKER 192.168.179.45
+ENV PORTMQTTBROKER 1883
+ENV MQTTTOPIC Vitoplus_300
+ENV MQTT true
+ENV MQTTPUB 30
 EXPOSE 3002/udp
 ENTRYPOINT ["sh","/etc/vcontrold/run.sh"]
--- root/etc/vcontrold/run.sh
+++ root/etc/vcontrold/run.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
-#/usr/local/edomi/main/vcontrol/vcontrold -x /usr/local/edomi/main/vcontrol/vcontrold.xml -d /dev/ttyUSB0 -l /usr/local/edomi/main/vcontrol/vcontrold.log -p 3002 >/dev/null 2>&1 & echo $!
-vcontrold -x /etc/vcontrold/vcontrold.xml -d /dev/ttyUSB0 -l /etc/vcontrold/vcontrold.log -p $PORTCLI
+# Konfiguration
+#MQTT=true
+#MQTTPUB=30
+
+vcontrold -x /etc/vcontrold/vcontrold.xml -d /dev/ttyUSB1 -P /var/run/vcontrold.pid
 status=$?
+pid=$(pidof vcontrold)
 if [ $status -ne 0 ];then
 	echo "Failed to start vcontrold"
-#	exit 0
-else
- pid=$(pidof vcontrold)
-# pid=$?
- echo "vcontrold gestartet PID: $pid"
 fi
-# Prüfintervall ob PIDfile vorhanden sonst Container beenden
-while sleep 5; do
-#	pidof vcontrold
-	pidnow=$(pidof vcontrold)
-	if [ $pid -eq $pidnow ]; then
-		echo "PID OK $pid = $pidnow"
-	else
-		echo "exit 0"
-#		exit 0
-	fi
-done
+
+if [ $MQTT = true ]; then
+	echo "vcontrold gestartet (PID $pid)"
+	echo "MQTT: aktiv (var = $MQTT)"
+	echo "Aktualisierungsintervall: $MQTTPUB sec"
+	while sleep $MQTTPUB; do
+		vclient -h 127.0.0.1 -p 3002 -f /etc/vcontrold/1_mqtt_commands.txt -t /etc/vcontrold/2_mqtt.tmpl -x /etc/vcontrold/3_mqtt_pub.sh
+		if [ -e /var/run/vcontrold.pid ]; then
+			:
+		else
+			echo "vcontrold.pid nicht vorhanden, exit 0"
+			exit 0
+		fi
+	done
+else
+	echo "vcontrold gestartet"
+	echo "MQTT: inaktiv (var = $MQTT)"
+	echo "PID: $pid"
+	while sleep 600; do
+		if [ -e /var/run/vcontrold.pid ]; then
+			:
+		else
+			echo "vcontrold.pid nicht vorhanden, exit 0"
+			exit 0
+		fi
+	done
+fi
